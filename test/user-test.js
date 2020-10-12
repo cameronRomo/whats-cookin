7c8e11ec52e25c3837f91cc0dda2382b4060d1aa
mdflynn <mdflynn34@outlook.com>
2020-10-12 14:34:59 -0600

    Initial commit

---
+++ test/user-test.js
@@ -0,0 +1,21 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const User = require('../src/User');
+
+describe('User', function() {
+    
+  let user;
+  beforeEach(() => {
+    user = new User();
+  })
+  
+  it('should be a function', function() {
+    expect(User).to.be.a('function');
+  });
+
+  it('should be an instance of User', function() {
+    expect(user).to.be.an.instanceof(User);
+  });
+
+});
