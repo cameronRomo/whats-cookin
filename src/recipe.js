aa776fc35b6d59835408b96f709a95a51c2ce0ed
mdflynn <mdflynn34@outlook.com>
2020-10-12 15:45:43 -0600

    Add name and tags property to Recipe

--- src/recipe.js
+++ src/recipe.js
@@ -1,10 +1,11 @@
 class Recipe {
-  constructor(id, image, ingredients, instructions, name) {
+  constructor(id, image, ingredients, instructions, name, tags) {
     this.id = id;
     this.image = image;
     this.ingredients = ingredients;
     this.instructions = instructions;
     this.name = name;
+    this.tags = tags
   }
 }
 
