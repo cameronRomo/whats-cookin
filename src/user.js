755edc06a0cd55eece8e2b9ce91bf23219f3c753
mdflynn <mdflynn34@outlook.com>
2020-10-13 17:29:28 -0600

    Add addToFavoriteRecipes method

--- src/user.js
+++ src/user.js
@@ -3,6 +3,16 @@ class User {
     this.favoriteRecipes = [];
     this.recipesToCook = [];
   } 
+
+  addToFavoriteRecipes(recipe) {
+    if (!recipe.favoritedRecipe) {
+      recipe.favoritedRecipe = true;
+      this.favoriteRecipes.push(recipe);
+    } else {
+      recipe.favoritedRecipe = true;
+      this.favoriteRecipes.splice(recipe, 1);
+    }
+  }
 }
 
 if (typeof module !== 'undefined') {
