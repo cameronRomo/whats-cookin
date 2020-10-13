65f006d00571dcdeb05c5f1a7228a6a18f0be36e
mdflynn <mdflynn34@outlook.com>
2020-10-13 17:40:29 -0600

    Add unfavorite test for User

--- test/user-test.js
+++ test/user-test.js
@@ -27,35 +27,51 @@ describe('User', function() {
     expect(user.recipesToCook).to.deep.equal([]);
   });
 
+});
+
+describe('User Methods', function() {
+    
+  let user;
+  let recipe1;
+  let id = 1;
+  let name = "Dirty Steve's Original Wing Sauce";
+  let image = "https://spoonacular.com/recipeImages/595736-556x370.jpg";
+  let instructions = [{
+    "instruction": "In a large mixing bowl, whisk together the dry ingredients (flour, pudding mix, soda and salt). Set aside.In a large mixing bowl of a stand mixer, cream butter for 30 seconds. Gradually add granulated sugar and brown sugar and cream until light and fluffy.",
+    "number": 1
+  },
+  {
+    "instruction": "Add egg and vanilla and mix until combined.",
+    "number": 2
+  }];
+  let ingredients = [{
+    "id": 20081,
+    "quantity": {
+      "amount": 1.5,
+      "unit": "c"
+    }},
+  {
+    "id": 18372,
+    "quantity": {
+      "amount": 2,
+      "unit": "c"
+    }
+  }];
+  let tags = ["sauce", "topping"];
+
+  beforeEach(() => {
+    recipe1 = new Recipe(id, image, ingredients, instructions, name, tags);
+    user = new User();
+  });
   it('should be able to favorite a recipe', function() {
-    let id = 1;
-    let name = "Dirty Steve's Original Wing Sauce";
-    let image = "https://spoonacular.com/recipeImages/595736-556x370.jpg";
-    let instructions = [{
-      "instruction": "In a large mixing bowl, whisk together the dry ingredients (flour, pudding mix, soda and salt). Set aside.In a large mixing bowl of a stand mixer, cream butter for 30 seconds. Gradually add granulated sugar and brown sugar and cream until light and fluffy.",
-      "number": 1
-    },
-    {
-      "instruction": "Add egg and vanilla and mix until combined.",
-      "number": 2
-    }];
-    let ingredients = [{
-      "id": 20081,
-      "quantity": {
-        "amount": 1.5,
-        "unit": "c"
-      }},
-    {
-      "id": 18372,
-      "quantity": {
-        "amount": 2,
-        "unit": "c"
-      }
-    }];
-    let tags = ["sauce", "topping"];
-    let recipe1 = new Recipe(id, image, ingredients, instructions, name, tags);
     user.addToFavoriteRecipes(recipe1);
     let recipeArray = [recipe1];
     expect(user.favoriteRecipes).to.deep.equal(recipeArray);
   });
+  
+  it('should remove a favorite recipe if unfavorited', function() {
+    recipe1.favoritedRecipe = true;
+    user.addToFavoriteRecipes(recipe1);
+    expect(user.favoriteRecipes).to.deep.equal([]);
+  })
 });
