bea85b430168b52da6d8aa73b3f38711829686e7
mdflynn <mdflynn34@outlook.com>
2020-10-17 16:28:24 -0600

    Add instructions to recipe modal

--- src/scripts.js
+++ src/scripts.js
@@ -1,10 +1,12 @@
 //const recipeData = require("../data/recipes");
 // Get the modal
 
+//const recipeData = require("../data/recipes");
+
 //const ingredientsData = require("../data/ingredients");
 
 //const ingredientsData = require("../data/ingredients");
-let modal; 
+let modal;
 function openModals(event) {
   let eventId = event.target.id;
   modal = document.getElementById(eventId + 'modal');
@@ -70,7 +72,11 @@ function showRecipes() {
   let recipeHTML = '';
   recipeInstantiation.forEach(recipe => {
     let recipeNames = recipe.getIngredients();
-
+    let showInstructions = recipe.getInstructions();
+    let instructionsHTML = showInstructions.reduce((instructionDetail, instruction) => {      
+      instructionDetail += `<li>${instruction}</li>`;
+      return instructionDetail;
+    }, '')
     let ingredientHTML = recipeNames.reduce((ingredientDetail, ingredient) => {
       ingredientDetail += `<li>${ingredient}</li>`;
       return ingredientDetail;
@@ -88,12 +94,7 @@ function showRecipes() {
                                   ${ingredientHTML}
                                 </ul>
                                 <ol>
-                                  <li>Preheat the oven to 325 degrees F.Coarsely chop the almonds and pecans.</li>
-                                  <li>Combine the oats, almonds, pecans, and salt in a bowl. Toss to combine.In a medium-sized bowl, combine the coconut oil (measure exactly when it's melted and not in the hardened state), and 1/2 cup chocolate chips.Microwave in bursts of 15 seconds stirring in between each burst for 15 seconds until completely melted.Stir in the brown sugar (measured lightly packed), honey or maple syrup, and vanilla extract.</li>
-                                  <li>Pour the chocolate wet mixture into the oat and nut mixture and stir until well combined.</li>
-                                  <li>Spread the granola evenly onto a parchment lined baking sheet.</li>
-                                  <li>Bake for 20-30 minutes (depending on the heat of your oven) making sure to flip and stir the granola around every 6-8 minutes.</li>
-                                  <li>Remove and allow the granola to harden and set up. (It may be soft and not very \"granola-like\", but it hardens as it dries out so be careful to not over-cook it). Mine generally takes about 21 minutes to be fully baked.Allow the granola to sit at room temperature for a few hours to harden and set up.Once the granola is hardened, stir in the remaining 1/2 cup chocolate chips and the toasted flaked coconut.To make a yogurt bowl: fill a bowl with the coconut cream yogurt, add a swirl of nut butter, add some fruit such as a banana, add the granola, and finish with chia seeds. Enjoy immediately.</li>
+                                  ${instructionsHTML}
                                 </ol>
                               </div>
                             </div>
@@ -103,5 +104,4 @@ function showRecipes() {
   })
   recipeSection.innerHTML = recipeHTML;
 }
-
 
